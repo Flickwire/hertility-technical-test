4a1a6c05e9d7eda6ee0c4dc6e9aeacb2e11a6eb1
Skye Brady <skye@blueskye.co.uk>
2025-08-13 16:12:34 +0100

    feat: add ui filter for results

--- frontend/src/components/Result.tsx
+++ frontend/src/components/Result.tsx
@@ -11,12 +11,13 @@ export const Result = ({
       <TableCell>{id}</TableCell>
       <TableCell>{userId}</TableCell>
       <TableCell className="font-medium">
-        {typeof normal !== "undefined" &&
-          (normal ? (
-            <span className="text-green-800 dark:text-green-500">IN RANGE</span>
-          ) : (
-            <span className="text-red-800 dark:text-red-500">NOT IN RANGE</span>
-          ))}
+        {typeof normal === "undefined" ? (
+          <span className="text-red-800 dark:text-red-500">UNKNOWN</span>
+        ) : normal ? (
+          <span className="text-green-800 dark:text-green-500">IN RANGE</span>
+        ) : (
+          <span className="text-red-800 dark:text-red-500">NOT IN RANGE</span>
+        )}
       </TableCell>
     </TableRow>
   );
--- frontend/src/components/Results.tsx
+++ frontend/src/components/Results.tsx
@@ -7,19 +7,38 @@ import {
 } from "@/components/ui/table";
 import { Result } from "./Result";
 import { ResultData } from "@/types/ResultData";
+import { useMemo, useState } from "react";
+import { ResultsFilterButton } from "./ResultsFilterButton";
 
 export const Results = ({ results }: { results: ResultData[] }) => {
+  const [filterType, setFilterType] = useState<
+    "unset" | "out-of-range" | "in-range"
+  >("unset");
+  const visibleResults = useMemo(() => {
+    return results.filter((result) => {
+      return (
+        filterType === "unset" ||
+        (filterType === "in-range" && result.normal) ||
+        (filterType === "out-of-range" && !result.normal)
+      );
+    });
+  }, [filterType, results]);
   return (
     <Table>
       <TableHeader>
         <TableRow>
           <TableHead className="text-center">result id</TableHead>
           <TableHead className="text-center">user id</TableHead>
-          <TableHead className="text-center">status</TableHead>
+          <TableHead className="text-center">
+            <ResultsFilterButton
+              filterType={filterType}
+              setFilterType={setFilterType}
+            />
+          </TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {results.map((result) => (
+        {visibleResults.map((result) => (
           <Result
             id={result.id}
             userId={result.userId}
