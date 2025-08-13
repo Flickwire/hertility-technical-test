af433523106ce81ad3ec70f429b4e41acc4e54cd
Skye Brady <skye@blueskye.co.uk>
2025-08-13 15:31:46 +0100

    feat(api): test hormone result range

--- backend/src/handlers/results.ts
+++ backend/src/handlers/results.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from "express";
-import { fetchResults } from "../services/results";
+import { fetchResults, fetchNormalRanges } from "../services/results";
 
 export const resultsHandler = async (_req: Request, res: Response) => {
     const results = await fetchResults();
-
+    const normalRanges = await fetchNormalRanges();
+    for (let i=0; i<results.length; i++) {
+        const result = results[i]!;
+        for (let j=0; j<result.hormoneResults.length; j++) {
+            const hormoneResult = result!.hormoneResults[j]!;
+            const normalRange = normalRanges[hormoneResult.code];
+            if (!normalRange) {
+                console.warn('Unknown Hormone', hormoneResult.code);
+                continue;
+            }
+            if (normalRange.min > hormoneResult.value) {
+                hormoneResult.range = 'BELOW';
+            } else if (normalRange.max < hormoneResult.value) {
+                hormoneResult.range = 'ABOVE';
+            } else {
+                hormoneResult.range = 'NORMAL';
+            }
+        }
+    }
     res.send(results);
 };
--- backend/src/services/results.ts
+++ backend/src/services/results.ts
@@ -2,6 +2,7 @@ interface HormoneResults {
     code: string;
     units: string;
     value: number;
+    range?: 'NORMAL' | 'BELOW' | 'ABOVE';
 }
 
 interface Results {
@@ -10,9 +11,19 @@ interface Results {
     hormoneResults: Array<HormoneResults>;
 }
 
+type NormalRange = Record<string, {min: number, max: number}>
+
 // this would normally be a database query - you don't need to change this function
 export async function fetchResults() {
     const json: { default: Results[] } = await import("../data/results.json", {
+        with: { type: "json" },
+    });
+	const results = json.default;
+    return results;
+}
+
+export async function fetchNormalRanges() {
+    const json: {default: NormalRange} = await import("../data/normal-ranges.json", {
         with: { type: "json" },
     });
 	const results = json.default;
